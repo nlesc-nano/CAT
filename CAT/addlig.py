7e557c8712dfe144e198f2d1c15b1e2525ff6995
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2019-02-08 15:13:06 +0100

    Dye builder initiator

---
+++ CAT/addlig.py
@@ -0,0 +1,47 @@
+from scm.plams.core.functions import read_molecules
+import time
+from .components.dye import bob, mono_di_substitution
+import os
+
+# Time measuring
+start = time.time()
+
+##### Input #####
+
+# Path to the working folder where are prepared molecules and where folder with new coordinares
+# will be made with the specific name
+working_folder_path = os.getcwd()
+
+input_ligands = read_molecules(os.path.join(working_folder_path, 'LIGANDS'))
+input_ligands = [bob(input_ligands[ligand]) for ligand in input_ligands]
+input_cores = read_molecules(os.path.join(working_folder_path, 'CORES'))
+input_cores = [bob(input_cores[core]) for core in input_cores]
+
+
+new_directory = 'new_molecules'
+distance_limit = 1.7 # if atoms are closer, geometry is rejected
+
+
+##############################################          new folder               ############################################
+
+# Makes new folder
+if not os.path.exists(os.path.join(working_folder_path, new_directory)):
+    os.makedirs(os.path.join(working_folder_path, new_directory))
+
+############################################ Monosubstitution and disubstitution ############################################
+
+new_molecules = mono_di_substitution(input_ligands, input_cores, distance_limit)
+# mono_subs contains sublists with plams molecule ('molecule') and binary ('check')
+# depending on binary value, False or True, name of the coordinate file is with or without 'err'.
+for item in new_molecules:
+    n = ['mono', 'di']
+    for molecule, check in item:
+        if check:
+            molecule.write(os.path.join(working_folder_path, new_directory, n[new_molecules.index(item)] + '_' + molecule.properties.name + '.xyz'))
+        else:
+            molecule.write(os.path.join(working_folder_path, new_directory, 'err_' + n[new_molecules.index(item)] + molecule.properties.name + '.xyz'))
+
+
+# The End
+end = time.time()
+print("Elapsed wall-clock time:  ", end - start)
