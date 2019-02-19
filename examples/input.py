cfa4fd3cc9007d409666a4b13a3c732de73ef554
Bas van Beek <b.f.van.beek@vu.nl>
2019-02-19 18:20:54 +0100

    Input files updated to accomdate new input interpretation

--- examples/input.py
+++ examples/input.py
@@ -1,36 +1,12 @@
 """ An example input file. """
 
-import os
 import yaml
-import CAT.base
+import CAT
+from scm.plams.core.settings import Settings
 
 
-# Mandatory arguments: input_cores, input ligands & path will have to be specified by the user
-path = '/Users/basvanbeek/Documents/CdSe/Week_5'
+yml = '/examples/input.yaml'
+with open(yml, 'r') as file:
+    arg = Settings(yaml.load(file))
 
-# The input cores from path/core/
-input_cores = yaml.load("""
--   - Cd68Se55.xyz
-    - guess_bonds: False
-""")
-
-# The input ligands from path/ligand/
-input_ligands = yaml.load("""
-- OC(C)=O
-""")
-
-# Optional arguments: these can safely be left to their default values
-argument_dict = {
-    'dir_name_list': ('core', 'ligand', 'QD'),
-    'dummy': 'Cl',
-    'use_database': True,
-    'ligand_opt': True,
-    'ligand_crs': False,
-    'qd_opt': False,
-    'qd_int': False,
-    'qd_dissociate': False,
-    'split': True,
-}
-
-# Runs the script: the ligand, core and quantum dot lists are returned
-qd_list, core_list, ligand_list = CAT.base.prep(input_ligands, input_cores, path, argument_dict)
+qd_list, core_list, ligand_list = CAT.base.prep(arg)
