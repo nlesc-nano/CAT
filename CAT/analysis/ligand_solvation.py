a714f1075c0d27f692d6ca840d5fe8f6e7b8a7bb
Bas van Beek <b.f.van.beek@vu.nl>
2019-03-04 18:11:49 +0100

    WiP: cosmo-rs to database

--- CAT/analysis/ligand_solvation.py
+++ CAT/analysis/ligand_solvation.py
@@ -5,7 +5,9 @@ __all__ = ['init_solv']
 import os
 from os.path import (join, dirname)
 
-from scm.plams.core.settings import Settings
+import numpy as np
+import pandas as pd
+
 from scm.plams.core.jobrunner import JobRunner
 from scm.plams.core.functions import (init, finish)
 from scm.plams.interfaces.adfsuite.adf import ADFJob
@@ -14,19 +16,32 @@ from .crs import CRSJob
 from .. import utils as CAT
 
 
-def init_solv(mol, job_recipe, solvent_list=None):
+def init_solv(mol_list, arg, solvent_list=None):
     """ Initialize the solvation energy calculation. """
+    # Prepare the job settings and solvent list
+    job_recipe = arg.optional.ligand.crs
     if solvent_list is None:
         path = join(join(dirname(dirname(__file__)), 'data'), 'coskf')
         solvent_list = [join(path, solv) for solv in os.listdir(path) if
                         solv not in ('__init__.py', 'README.rst')]
+    solvent_list.sort()
 
-    init(path=mol.properties.path, folder='ligand_solvation')
-    coskf = get_surface_charge(mol, job=job_recipe.job1, s=job_recipe.s1)
-    solv_dict = get_solv(mol, solvent_list, coskf, job=job_recipe.job2, s=job_recipe.s2)
-    mol.properties.energy.E_solv = solv_dict.solv
-    mol.properties.gamma_solv = solv_dict.gamma
+    # Prepare the dataframe
+    index = ['E_solv ' + i.rsplit('.', 1)[0].rsplit('/', 1)[-1] for i in solvent_list]
+    index += ['Gamma ' + i.rsplit('.', 1)[0].rsplit('/', 1)[-1] for i in solvent_list]
+    df = pd.DataFrame(index=index)
+
+    # Run COSMO-RS
+    init(path=mol_list[0].properties.path, folder='ligand_solvation')
+    for mol in mol_list:
+        coskf = get_surface_charge(mol, job=job_recipe.job1, s=job_recipe.s1)
+        E_solv = get_solv(mol, solvent_list, coskf, job=job_recipe.job2, s=job_recipe.s2)
+        df[mol.properties.name] = E_solv
     finish()
+
+    # Update the database
+    if arg.properties.database.write:
+        pass
 
 
 def get_surface_charge(mol, job=None, s=None):
@@ -57,19 +72,15 @@ def get_solv(mol, solvent_list, coskf, job=None, s=None):
     results = [job.run(jobrunner=JobRunner(parallel=True)) for job in jobs]
 
     # Extract solvation energies and activity coefficients
-    solv_dict = {}
-    activ_dict = {}
+    E_solv = []
+    Gamma = []
     for result in results:
         result.wait()
-        name = result.job.settings.name
-        solv_dict[name] = result.get_energy()
-        activ_dict[name] = result.get_activity_coefficient()
+        E_solv.append(result.get_energy())
+        Gamma.append(result.get_activity_coefficient())
 
-    # Return the solvation energies and activity coefficients as Settings object
-    s = Settings()
-    s.solv = solv_dict
-    s.gamma = activ_dict
-    return s
+    # Return the solvation energies and activity coefficients as dict
+    return np.array(E_solv + Gamma)
 
 
 def get_surface_charge_adf(mol, job, s):
