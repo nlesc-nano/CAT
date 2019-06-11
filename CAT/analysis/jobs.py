af0bf499d43e0a48ce2e86c2a37b1dc2d834b6ab
Bas van Beek <b.f.van.beek@vu.nl>
2019-06-11 15:34:11 +0200

    Fixed molecular charges when running jobs

--- CAT/analysis/jobs.py
+++ CAT/analysis/jobs.py
@@ -51,7 +51,7 @@ def job_single_point(self, job, settings, name='Single_point', ret_results=False
     s.update(settings)
     if job == AMSJob:
         s.input.ams.system.bondorders._1 = adf_connectivity(self)
-        if not self.properties.charge and 'uff' not in s.input:
+        if 'uff' not in s.input:
             s.input.ams.system.charge = sum([at.properties.charge for at in self if
                                              'charge' in at.properties])
 
@@ -87,7 +87,7 @@ def job_geometry_opt(self, job, settings, name='Geometry_optimization', ret_resu
     s.update(settings)
     if job == AMSJob:
         s.input.ams.system.bondorders._1 = adf_connectivity(self)
-        if not self.properties.charge and 'uff' not in s.input:
+        if 'uff' not in s.input:
             s.input.ams.system.charge = sum([at.properties.charge for at in self if
                                              'charge' in at.properties])
 
@@ -131,7 +131,7 @@ def job_freq(self, job, settings, name='Frequency_analysis', opt=True, ret_resul
     s.update(settings)
     if job == AMSJob:
         s.input.ams.system.bondorders._1 = adf_connectivity(self)
-        if not self.properties.charge and 'uff' not in s.input:
+        if 'uff' not in s.input:
             s.input.ams.system.charge = sum([at.properties.charge for at in self if
                                              'charge' in at.properties])
 
