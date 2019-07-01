92c0b829f364305178c912d063a7071735950226
Bas van Beek <b.f.van.beek@vu.nl>
2019-07-01 18:41:30 +0200

    Update ligand_solvation.py

--- CAT/analysis/ligand_solvation.py
+++ CAT/analysis/ligand_solvation.py
@@ -4,7 +4,7 @@ import os
 import shutil
 from itertools import product
 from os.path import (join, dirname)
-from typing import (Optional, Sequence, Callable, Container, Tuple, List)
+from typing import (Optional, Sequence, Callable, Container, Tuple, List, Iterable)
 
 import numpy as np
 import pandas as pd
@@ -50,29 +50,16 @@ def init_solv(ligand_df: PropertiesDataFrame,
     """
     # Unpack arguments
     properties = ligand_df.properties
-    path = ligand_df['mol'][0].properties.path
     overwrite = 'ligand' in properties.optional.database.overwrite
     read = 'ligand' in properties.optional.database.read
     write = 'ligand' in properties.optional.database.write
-    j1 = properties.optional.ligand.crs.job1
-    j2 = properties.optional.ligand.crs.job2
-    s1 = properties.optional.ligand.crs.s1
-    s2 = properties.optional.ligand.crs.s2
-
     data = Database(path=properties.optional.database.dirname)
 
     # Prepare the job settings and solvent list
-    if solvent_list is None:
-        coskf_path = join(join(dirname(dirname(__file__)), 'data'), 'coskf')
-        solvent_list = [join(coskf_path, solv) for solv in os.listdir(coskf_path) if
-                        solv not in ('__init__.py', 'README.rst')]
-    solvent_list.sort()
+    solvent_list = get_solvent_list(solvent_list)
 
     # Update the columns of **ligand_df**
-    columns = [i.rsplit('.', 1)[0].rsplit('/', 1)[-1] for i in solvent_list]
-    columns = list(product(('E_solv', 'gamma'), columns))
-    for item in columns:
-        ligand_df[item] = np.nan
+    columns = update_columns(ligand_df, solvent_list)
 
     # Check if the calculation has been done already
     if not overwrite and read:
@@ -81,21 +68,8 @@ def init_solv(ligand_df: PropertiesDataFrame,
     # Run COSMO-RS
     idx = ligand_df[['E_solv', 'gamma']].isna().all(axis='columns')
     if idx.any():
-        init(path=path, folder='ligand_solvation')
-        for i, mol in ligand_df[MOL][idx].iteritems():
-            mol.properties.job_path = []
-            coskf = get_surface_charge(mol, job=j1, s=s1)
-            e_and_gamma = get_solv(mol, solvent_list, coskf, job=j2, s=s2)
-            ligand_df.loc[i, 'E_solv'], ligand_df.loc[i, 'gamma'] = e_and_gamma
-        finish()
-
-        job_settings = []
-        for mol in ligand_df['mol']:
-            try:
-                job_settings.append(mol.properties.pop('job_path'))
-            except KeyError:
-                job_settings.append([])
-        ligand_df[JOB_SETTINGS_CRS] = job_settings
+        start_crs_jobs(ligand_df)
+        ligand_df[JOB_SETTINGS_CRS] = get_job_settings()
     else:
         return None  # No new molecules here; move along
 
@@ -104,6 +78,58 @@ def init_solv(ligand_df: PropertiesDataFrame,
         with pd.option_context('mode.chained_assignment', None):
             _ligand_to_db(ligand_df, idx, columns)
     return None
+
+
+def start_crs_jobs(ligand_df: PropertiesDataFrame,
+                   idx: pd.Series,
+                   solvent_list: Iterable[str]) -> None:
+    """Loop over all molecules in ``ligand_df.loc[idx]`` and perform COSMO-RS calculations."""
+    # Unpack arguments
+    path = ligand_df.properties.optional.ligand.dirname
+    j1 = ligand_df.properties.optional.ligand.crs.job1
+    j2 = ligand_df.properties.optional.ligand.crs.job2
+    s1 = ligand_df.properties.optional.ligand.crs.s1
+    s2 = ligand_df.properties.optional.ligand.crs.s2
+
+    # Start the main loop
+    init(path=path, folder='ligand_solvation')
+    for i, mol in ligand_df[MOL][idx].iteritems():
+        mol.properties.job_path = []
+        coskf = get_surface_charge(mol, job=j1, s=s1)
+        e_and_gamma = get_solv(mol, solvent_list, coskf, job=j2, s=s2)
+        ligand_df.loc[i, 'E_solv'], ligand_df.loc[i, 'gamma'] = e_and_gamma
+    finish()
+
+
+def update_columns(ligand_df: PropertiesDataFrame,
+                   solvent_list: Iterable[str]) -> List[Tuple[str, str]]:
+    """Add all COSMO-RS related columns to **ligand_df**."""
+    clm_tups = [i.rsplit('.', 1)[0].rsplit('/', 1)[-1] for i in solvent_list]
+    columns = list(product(('E_solv', 'gamma'), clm_tups))
+    for item in columns:
+        ligand_df[item] = np.nan
+    return columns
+
+
+def get_solvent_list(solvent_list: Optional[Sequence[str]]) -> Sequence[str]:
+    """Construct the list of solvents."""
+    if solvent_list is None:
+        coskf_path = join(join(dirname(dirname(__file__)), 'data'), 'coskf')
+        solvent_list = [join(coskf_path, solv) for solv in os.listdir(coskf_path) if
+                        solv not in ('__init__.py', 'README.rst')]
+    solvent_list.sort()
+    return solvent_list
+
+
+def get_job_settings(ligand_df: PropertiesDataFrame) -> List[str]:
+    """Create a nested list of input files for each molecule in **ligand_df**."""
+    job_settings = []
+    for mol in ligand_df[MOL]:
+        try:
+            job_settings.append(mol.properties.pop('job_path'))
+        except KeyError:
+            job_settings.append([])
+    return job_settings
 
 
 def _ligand_to_db(ligand_df: PropertiesDataFrame,
@@ -167,7 +193,7 @@ def get_surface_charge(mol: Molecule,
 
 
 def get_solv(mol: Molecule,
-             solvent_list: Sequence[str],
+             solvent_list: Iterable[str],
              coskf: str,
              job: Callable,
              s: Settings,
