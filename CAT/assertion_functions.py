de10b3f82308f2db0a31739c6e0d824e81057b19
Bas van Beek <b.f.van.beek@vu.nl>
2019-07-08 22:16:58 +0200

    Update assertion_functions.py

--- CAT/assertion_functions.py
+++ CAT/assertion_functions.py
@@ -8,6 +8,7 @@ Index
 -----
 .. currentmodule:: CAT.assertion_functions
 .. autosummary::
+    invert
     assert_value
     assert_id
     assert_instance
@@ -15,10 +16,12 @@ Index
     assert_isin
     assert_lt
     assert_gt
-
+    _err_msg
+    _exc_msg
 
 API
 ---
+.. autoclass:: CAT.assertion_functions.Invert
 .. autofunction:: CAT.assertion_functions.assert_value
 .. autofunction:: CAT.assertion_functions.assert_id
 .. autofunction:: CAT.assertion_functions.assert_instance
@@ -26,12 +29,13 @@ API
 .. autofunction:: CAT.assertion_functions.assert_isin
 .. autofunction:: CAT.assertion_functions.assert_lt
 .. autofunction:: CAT.assertion_functions.assert_gt
-
+.. autofunction:: CAT.assertion_functions._err_msg
+.. autofunction:: CAT.assertion_functions._exc_msg
 
 """
 
 from functools import wraps
-from typing import (Hashable, Any, Callable, Tuple, Sequence, Container)
+from typing import (Any, Callable, Tuple, Sequence, Container)
 
 
 class Invert():
@@ -64,29 +68,10 @@ class Invert():
         A callable constructed from the **func** parameter.
         Operations that previously did *not* raise an :exc:`AssertionError` now do and *vice versa*.
 
-    err_dict : |dict|_ [|Callable|_, |str|_]
-        A dictionary with error messages for specific functions:
-            * :func:`.assert_value`
-            * :func:`.assert_id`
-            * :func:`.assert_instance`
-            * :func:`.assert_exception`
-            * :func:`.assert_isin`
-            * :func:`.assert_lt`
-            * :func:`.assert_gt`
-
     """
 
     def __init__(self, func: Callable) -> None:
         """Initialize the :class:`.Invert` context manager."""
-        self.err_dict = {
-            assert_value: '\ninvalid value observed:\t{}',
-            assert_id: '\ninvalid id observed:\t{}',
-            assert_instance: '\ninvalid instance observed:\t{}',
-            assert_exception: '\ninvalid exception observed:\t{}',
-            assert_isin: '\nvalue:\t{}\n\nis in:\t{}',
-            assert_lt: '\nvalue:\t{}\n\nis smaller than:\t{}',
-            assert_gt: '\nvalue\t{}\n\nis larger than:\t{}',
-        }
         self.func = self.invert(func)
 
     def __enter__(self) -> Callable:
@@ -95,70 +80,92 @@ class Invert():
 
     def __exit__(self, *args) -> None:
         """Close the :class:`.Invert` context manager."""
-        pass
+        return
+
+    def get_err_msg(self, func: Callable,
+                    args: Tuple[str, Any, Any]) -> str:
+        """Create an error message for :meth:`Invert.invert`."""
+        if not args:
+            return ''
+        elif func is assert_exception:
+            return _exc_msg(*args)
+        else:
+            return _err_msg(*args)
 
     def invert(self, func: Callable) -> Callable:
         """Invert a function that may or may not raise an :exc:`AssertionError`."""
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                i = func(*args, **kwargs)
+                tup = func(*args, **kwargs)
             except AssertionError:
                 pass
             else:
-                raise AssertionError(self.err_dict.setdefault(func, '').format(*i))
+                raise AssertionError(self.get_err_msg(func, *tup))
         return wrapper
 
 
-def assert_lt(value: Any, ref) -> Tuple[str, str]:
-    """Assert :code:`value < ref`; returns a tuple of **value** and **ref**."""
-    assert value < ref, f'\nvalue:\t{repr(value)}\n\nis larger than:\t{repr(value)}'
-    return repr(value), repr(ref)
+def assert_lt(value: Any,
+              ref: Any) -> Tuple[str, Any, Any]:
+    """Assert :code:`value < ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value < reference'
+    assert value < ref, _err_msg(assertion, value, ref)
+    return assertion, value, ref
 
 
-def assert_gt(value: Any, ref) -> Tuple[str, str]:
-    """Assert :code:`value > ref`; returns a tuple of **value** and **ref**."""
-    assert value > ref, f'\nvalue\t{repr(value)}\n\nis smaller than:\t{repr(value)}'
-    return repr(value), repr(ref)
+def assert_gt(value: Any,
+              ref: Any) -> Tuple[str, Any, Any]:
+    """Assert :code:`value > ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value > reference'
+    assert value > ref, _err_msg(assertion, value, ref)
+    return assertion, value, ref
 
 
-def assert_isin(value: Any, ref: Container) -> str:
-    """Assert :code:`value in ref`; returns **value** as :class:`str`."""
-    assert value in ref, f'\nvalue\t{repr(value)}\n\nis not in:\t{repr(ref)}'
-    return repr(value)
+def assert_isin(value: Any,
+                ref: Container) -> Tuple[str, Any, Container]:
+    """Assert :code:`value in ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value is in reference'
+    assert value in ref, _err_msg(assertion, value, ref)
+    return assertion, value, ref
 
 
-def assert_instance(value: Any, ref: Callable) -> str:
-    """Assert :code:`isinstance(value, ref)`; returns the class name of **value**."""
-    ref_name = repr(ref.__name__)
-    value_name = repr(value.__class__.__name__)
-    err = f'\ninstance expected:\t{ref_name}; \n\ninstance observed:\t{value_name}'
-    assert isinstance(value, ref), err
-    return value_name
+def assert_instance(value: Any,
+                    ref: type) -> Tuple[str, str, str]:
+    """Assert :code:`isinstance(value, ref)`; returns arguments for :func:`._err_msg`."""
+    assertion = 'isinstance(value, reference)'
+    ref_name = ref.__name__
+    value_name = value.__class__.__name__
+    assert isinstance(value, ref), _err_msg(assertion, ref_name, value_name)
+    return assertion, ref_name, value_name
 
 
-def assert_value(value: Hashable,
-                 ref: Hashable) -> str:
-    """Assert :code:`value == ref`; returns **value** as :class:`str`."""
-    assert value == ref, f'\nvalue expected:\t{repr(ref)}; \n\nvalue observed:\t{repr(value)}'
-    return repr(value)
+def assert_value(value: Any,
+                 ref: Any) -> Tuple[str, Any, Any]:
+    """Assert :code:`value == ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value == reference'
+    assert value == ref, _err_msg(assertion, value, ref)
+    return assertion, value, ref
 
 
-def assert_id(value: Hashable,
-              ref: Hashable) -> str:
-    """Assert :code:`value is ref`; returns the :class:`id` of **value**."""
+def assert_id(value: Any,
+              ref: Any) -> Tuple[str, int, int]:
+    """Assert :code:`value is ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value is reference'
     assert_value(ref, value)
-    assert ref is value, f'\nid expected:\t{repr(id(ref))}; \n\nid observed:\t{repr(id(value))}'
-    return repr(id(value))
+    value_id = f'{id(value)} = id({value})'
+    ref_id = f'{id(ref)} = id({ref})'
+    assert ref is value, _err_msg(assertion, value_id, ref_id)
+    return assertion, value_id, ref_id
 
 
 def assert_exception(exc: Exception,
                      func: Callable,
                      *args: Sequence,
-                     **kwargs: dict) -> str:
-    """Assert that :code:`func(*args, **kwargs)` raises **exc**; returns the name of **exc**."""
-    err_ref = repr(exc.__name__)
-    err = repr(None.__class__.__name__)
+                     **kwargs: dict) -> Tuple[str, str, str]:
+    """Assert that :code:`func(*args, **kwargs)` raises **exc**."""
+    err_ref = exc.__name__
+    err = None.__class__.__name__
+    assertion = f'{func}(*{args}, **{kwargs}) -> {exc.__name__}'
 
     try:
         func(*args, **kwargs)
@@ -166,7 +173,25 @@ def assert_exception(exc: Exception,
         pass
     except Exception as ex:  # An undesired exception is raised
         err = repr(ex.__class__.__name__)
-        raise AssertionError(f'\nexception expected:\t{err_ref}; \n\nexception observed:\t{err}')
+        raise AssertionError(_exc_msg(assertion, err, err_ref))
     else:  # No exception is raised; this is undesired
-        raise AssertionError(f'\nexception expected:\t{err_ref}; \n\nexception observed:\t{err}')
-    return err_ref
+        raise AssertionError(_exc_msg(assertion, err, err_ref))
+
+    _assertion = f'{func}(*{args}, **{kwargs}) -/> {exc.__name__}'
+    return _assertion, err, err_ref
+
+
+def _err_msg(assertion: Any,
+             value: Any,
+             ref: Any) -> str:
+    """Return a formatted error message."""
+    i, j, k = repr(assertion), repr(value), repr(ref)
+    return f'{i}\nSupplied value:\n\t{j}\n\nSupplied reference:\n\t{k}'
+
+
+def _exc_msg(assertion: Any,
+             value: Any,
+             ref: Any) -> str:
+    """Return a formatted error message for :func:`.assert_exception`."""
+    i, j, k = repr(assertion), repr(value), repr(ref)
+    return f'{i}\nSupplied exception:\n\t{j}\n\nReference exception:\n\t{k}'
