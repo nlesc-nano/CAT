352938f4df58aebbf4747f90c69250e023236eca
Bas van Beek <b.f.van.beek@vu.nl>
2019-07-09 17:51:29 +0200

    Update

--- CAT/assertion_functions.py
+++ CAT/assertion_functions.py
@@ -8,14 +8,16 @@ Index
 -----
 .. currentmodule:: CAT.assertion_functions
 .. autosummary::
-    invert
+    Invert
     assert_value
     assert_id
     assert_instance
     assert_exception
     assert_isin
     assert_lt
+    assert_lte
     assert_gt
+    assert_gte
     _err_msg
     _exc_msg
 
@@ -28,7 +30,9 @@ API
 .. autofunction:: CAT.assertion_functions.assert_exception
 .. autofunction:: CAT.assertion_functions.assert_isin
 .. autofunction:: CAT.assertion_functions.assert_lt
+.. autofunction:: CAT.assertion_functions.assert_lte
 .. autofunction:: CAT.assertion_functions.assert_gt
+.. autofunction:: CAT.assertion_functions.assert_gte
 .. autofunction:: CAT.assertion_functions._err_msg
 .. autofunction:: CAT.assertion_functions._exc_msg
 
@@ -64,7 +68,7 @@ class Invert():
 
     Attributes
     ----------
-    func: |Callable|_
+    func : |Callable|_
         A callable constructed from the **func** parameter.
         Operations that previously did *not* raise an :exc:`AssertionError` now do and *vice versa*.
 
@@ -105,12 +109,34 @@ class Invert():
         return wrapper
 
 
+def assert_lte(value: Any,
+               ref: Any) -> Tuple[str, Any, Any]:
+    """Assert :code:`value <= ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value <= reference'
+    assert value <= ref, _err_msg(assertion, value, ref)
+
+    _assertion = 'value > reference'
+    return _assertion, value, ref
+
+
+def assert_gte(value: Any,
+               ref: Any) -> Tuple[str, Any, Any]:
+    """Assert :code:`value >= ref`; returns arguments for :func:`._err_msg`."""
+    assertion = 'value => reference'
+    assert value >= ref, _err_msg(assertion, value, ref)
+
+    _assertion = 'value < reference'
+    return _assertion, value, ref
+
+
 def assert_lt(value: Any,
               ref: Any) -> Tuple[str, Any, Any]:
     """Assert :code:`value < ref`; returns arguments for :func:`._err_msg`."""
     assertion = 'value < reference'
     assert value < ref, _err_msg(assertion, value, ref)
-    return assertion, value, ref
+
+    _assertion = 'value >= reference'
+    return _assertion, value, ref
 
 
 def assert_gt(value: Any,
@@ -118,15 +144,19 @@ def assert_gt(value: Any,
     """Assert :code:`value > ref`; returns arguments for :func:`._err_msg`."""
     assertion = 'value > reference'
     assert value > ref, _err_msg(assertion, value, ref)
-    return assertion, value, ref
+
+    _assertion = 'value <= reference'
+    return _assertion, value, ref
 
 
 def assert_isin(value: Any,
                 ref: Container) -> Tuple[str, Any, Container]:
     """Assert :code:`value in ref`; returns arguments for :func:`._err_msg`."""
-    assertion = 'value is in reference'
+    assertion = 'value in reference'
     assert value in ref, _err_msg(assertion, value, ref)
-    return assertion, value, ref
+
+    _assertion = 'value not in reference'
+    return _assertion, value, ref
 
 
 def assert_instance(value: Any,
@@ -136,7 +166,9 @@ def assert_instance(value: Any,
     ref_name = ref.__name__
     value_name = value.__class__.__name__
     assert isinstance(value, ref), _err_msg(assertion, ref_name, value_name)
-    return assertion, ref_name, value_name
+
+    _assertion = 'not isinstance(value, reference)'
+    return _assertion, ref_name, value_name
 
 
 def assert_value(value: Any,
@@ -144,7 +176,9 @@ def assert_value(value: Any,
     """Assert :code:`value == ref`; returns arguments for :func:`._err_msg`."""
     assertion = 'value == reference'
     assert value == ref, _err_msg(assertion, value, ref)
-    return assertion, value, ref
+
+    _assertion = 'value != reference'
+    return _assertion, value, ref
 
 
 def assert_id(value: Any,
@@ -155,7 +189,9 @@ def assert_id(value: Any,
     value_id = f'{id(value)} = id({value})'
     ref_id = f'{id(ref)} = id({ref})'
     assert ref is value, _err_msg(assertion, value_id, ref_id)
-    return assertion, value_id, ref_id
+
+    _assertion = 'value is not reference'
+    return _assertion, value_id, ref_id
 
 
 def assert_exception(exc: Exception,
@@ -165,19 +201,24 @@ def assert_exception(exc: Exception,
     """Assert that :code:`func(*args, **kwargs)` raises **exc**."""
     err_ref = exc.__name__
     err = None.__class__.__name__
-    assertion = f'{func}(*{args}, **{kwargs}) -> {exc.__name__}'
+    arguments = ''
+    if args is not None:
+        arguments += ', '.join(repr(i) for i in args)
+    if kwargs is not None:
+        arguments += ', '.join(f'{k}={v}' for k, v in kwargs.items())
+    assertion = f'{func.__qualname__}({arguments}) -> {exc.__name__}'
 
     try:
         func(*args, **kwargs)
     except exc:  # The desired exception is raised
         pass
     except Exception as ex:  # An undesired exception is raised
-        err = repr(ex.__class__.__name__)
+        err = ex.__class__.__name__
         raise AssertionError(_exc_msg(assertion, err, err_ref))
     else:  # No exception is raised; this is undesired
         raise AssertionError(_exc_msg(assertion, err, err_ref))
 
-    _assertion = f'{func}(*{args}, **{kwargs}) -/> {exc.__name__}'
+    _assertion = f'{func.__qualname__}({arguments}) -/> {exc.__name__}'
     return _assertion, err, err_ref
 
 
