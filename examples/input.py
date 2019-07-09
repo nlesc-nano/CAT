cbf6106119ea2fed64d793c5be200b690260c279
Bas van Beek <b.f.van.beek@vu.nl>
2019-07-09 17:51:22 +0200

    Fixed import path

--- examples/input.py
+++ examples/input.py
@@ -5,11 +5,11 @@ from os.path import (dirname, join)
 import yaml
 from scm.plams import Settings
 
-import CAT
+from CAT import base
 
 
 yaml_path = join(dirname(__file__), 'input_settings.yaml')
 with open(yaml_path, 'r') as file:
     arg = Settings(yaml.load(file, Loader=yaml.FullLoader))
 
-qd_df, core_df, ligand_df = CAT.base.prep(arg)
+qd_df, core_df, ligand_df = base.prep(arg)
