9f5fc8b816bab3d5463e6aa0ccdc0e403053090a
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2019-07-12 14:24:32 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,12 +25,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
+needs_sphinx = '2.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings. 
+# They can be extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
@@ -47,9 +45,7 @@ extensions = [
 templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
+# You can specify multiple suffix as a list of string: source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
 # The master toctree document.
