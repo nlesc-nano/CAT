31269941555f460b43201341a959f2d7862b93ba
Bas van Beek <b.f.van.beek@vu.nl>
2019-12-24 22:27:23 +0100

    Added tests for the new ``distribution`` module

--- CAT/attachment/distribution.py
+++ CAT/attachment/distribution.py
@@ -20,9 +20,8 @@ API
 
 """
 
-import random
 import reprlib
-from typing import Generator, Optional, Iterable, TypeVar, FrozenSet, Any, Union
+from typing import Generator, Optional, Iterable, FrozenSet, Any, Union
 from itertools import islice
 
 import numpy as np
@@ -32,15 +31,12 @@ from scm.plams import Molecule
 
 __all__ = ['distribute_idx']
 
-
 #: A set of allowed values for the **mode** parameter in :func:`get_distribution`.
 MODE_SET: FrozenSet[str] = frozenset({'uniform', 'random', 'cluster'})
 
-T = TypeVar('T')
 
-
-def distribute_idx(core: Union[Molecule, np.ndarray], idx: Union[int, Iterable[int]],
-                   mode: str = 'uniform', p: float = 0.5, **kwargs: Any) -> np.ndarray:
+def distribute_idx(core: Union[Molecule, np.ndarray], idx: Union[int, Iterable[int]], p: float,
+                   mode: str = 'uniform', **kwargs: Any) -> np.ndarray:
     r"""Create a new distribution of atomic indices from **idx** of length :code:`p * len(idx)`.
 
     Parameters
@@ -53,6 +49,10 @@ def distribute_idx(core: Union[Molecule, np.ndarray], idx: Union[int, Iterable[i
         An integer or iterable of integers representing the 0-based indices of
         all anchor atoms in **core**.
 
+    p : :class:`float`
+        A float obeying the following condition: :math:`0.0 < p <= 1.0`.
+        Represents the fraction of **idx** that will be returned.
+
     mode : :class:`str`
         How the subset of to-be returned indices will be generated.
         Accepts one of the following values:
@@ -62,10 +62,6 @@ def distribute_idx(core: Union[Molecule, np.ndarray], idx: Union[int, Iterable[i
           all previous points is maximized.
         * ``"cluster"``: A clustered distribution; the distance between each successive point and
           all previous points is minmized.
-
-    p : :class:`float`
-        A float obeying the following condition: :math:`0.0 < p <= 1.0`.
-        Represents the fraction of **idx** that will be returned.
 
     \**kwargs : :data:`Any<typing.Any>`
         Further keyword arguments for the **mode**-specific functions.
@@ -106,15 +102,14 @@ def distribute_idx(core: Union[Molecule, np.ndarray], idx: Union[int, Iterable[i
     if mode in ('uniform', 'cluster'):
         xyz = np.array(core, dtype=float, ndmin=2, copy=False)[idx_ar]
         dist = cdist(xyz, xyz)
-        operation = 'max' if 'cluster' else 'min'
-        start = kwargs.get('start', None)
-        generator = (idx_ar[i] for i in uniform_idx(dist, operation, start=start))
+        operation = 'max' if mode == 'cluster' else 'min'
+        iterable = (idx_ar[i] for i in uniform_idx(dist, operation, kwargs.get('start', None)))
     elif mode == 'random':
-        generator = random_idx(idx_ar)
+        iterable = np.random.permutation(idx_ar)
 
     # Return a list of `p * len(idx)` atomic indices
     stop = max(1, round(p * len(idx_ar)))
-    return np.fromiter((i for i in islice(generator, stop)), count=p, dtype=int)
+    return np.fromiter(islice(iterable, stop), count=stop, dtype=int)
 
 
 def uniform_idx(dist: np.ndarray, operation: str = 'max',
@@ -151,7 +146,7 @@ def uniform_idx(dist: np.ndarray, operation: str = 'max',
 
     start : :class:`int`, optional
         The index of the starting row in **dist**.
-        Will be randomized if ``None``.
+        If ``None``, start in whichever row contains the global minimum/maximum (see **operation**).
 
     Yields
     ------
@@ -162,18 +157,19 @@ def uniform_idx(dist: np.ndarray, operation: str = 'max',
     if operation not in ('min', 'max'):
         raise ValueError(f"Invalid value for 'mode' ({reprlib.repr(operation)}); "
                          f"accepted values: ('min', 'max')")
-
-    shift = start if start is not None else random.randint(0, len(dist)-1)
     dist_sqr = np.asarray(dist)**2  # Squared distance matrix
-    dist_sqr[:] = np.roll(dist_sqr, shift, axis=0)  # Shift the first row to **shift**
-    norm_sqr: float = 0.0  # Squared norm
+    norm_sqr = 0.0  # Squared norm
 
     # Use either argmin or argmax
     if operation == 'min':
+        fill_value = np.inf
         np.fill_diagonal(dist_sqr, np.inf)
         arg_func = np.ndarray.argmin
     else:
+        fill_value = 0.0
         arg_func = np.ndarray.argmax
+    shift = start if start is not None else np.unravel_index(arg_func(dist), dist.shape)[0]
+    dist_sqr[:] = np.roll(dist_sqr, shift, axis=0)  # Shift the first row to **shift**
 
     # Yield indices
     for i, ar in enumerate(dist_sqr):
@@ -182,32 +178,5 @@ def uniform_idx(dist: np.ndarray, operation: str = 'max',
         j = arg_func(ar_sqrt)
         norm_sqr += ar[j]
 
-        dist_sqr[:, j] = 0.0
+        dist_sqr[:, j] = fill_value
         yield j
-
-
-def random_idx(iterable: Iterable[T]) -> Generator[T, None, None]:
-    r"""Yield random elements from an **iterable**.
-
-    No single element is returned more than once.
-
-    Parameters
-    ----------
-    iterable : :class:`Iterable<collections.abc.Iterable>`
-        An iterable.
-
-    Yields
-    ------
-    :class:`object`
-        Random elements from **iterable**.
-
-    """
-    try:
-        k = len(iterable)
-    except TypeError:  # It's an iterator
-        iterable = list(iterable)
-        k = len(iterable)
-
-    rand_element = random.sample(iterable, k=k)
-    for i in rand_element:
-        yield i
