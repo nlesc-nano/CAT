ebceae86caea86839764612882201eff5250356d
JBelic <jbelic20@gmail.com>
2020-05-07 12:27:33 +0200

    Attachment and  xyz export
    - new arguments ’n’ and ‘symmetry’
    - documentation

--- CAT/dye/addlig.py
+++ CAT/dye/addlig.py
@@ -2,7 +2,7 @@
 CAT.dye.addlig
 ==============
 
-TODO: Insert a short description of the module
+A module for multiple compound attachment and export of the xyz files.
 
 Index
 -----
@@ -18,7 +18,6 @@ API
 
 """
 
-import time
 import os
 from itertools import chain
 from typing import Iterator, Iterable
@@ -32,54 +31,62 @@ from CAT.attachment.substitution_symmetry import del_equiv_structures
 
 __all__ = ['add_ligands', 'export_dyes']
 
-"""
-path = join(CAT.__path__[0], 'data')
-core_dir = join(path, 'CORES')
-ligand_dir = join(path, 'LIGANDS')
-"""
 
+def add_ligands(core_dir: str, ligand_dir: str, min_dist: float = 1.2, n: int = 1, symmetry: Tuple[str] = []) -> Iterator[Molecule]:
+    """Add ligand(s) to one core.
 
-def add_ligands(core_dir: str, ligand_dir: str, min_dist: float = 1.2) -> Iterator[Molecule]:
-    # Time measuring
-    start = time.time()
+    Parameters
+    ----------
+    core_dir: str
+        Name of directory where core coordinates are located
+    ligand_dir: str
+        Name of directory where ligands coordinates are located
+    min_dist: float
+        Criterion for the minimal interatomic distances
+    n: int
+        Number of substitutions
+    symmetry: tulpe[str]
+        Keywords for substitution symmetry for deleting equivalent molecules
 
-    # Preparing molecules = in the comment section of xyz file,
-    # write serial numbers of atoms that will be substituted
+    Returns
+    -------
+    Iterator[Molecule]
+        New structures that are containg core and lingad fragments
 
-    # Path to the working folder where are prepared molecules and where folder with new coordinares
-    # will be made with the specific name
+    """
     input_ligands = list(read_molecules(ligand_dir).values())
     input_cores = list(read_molecules(core_dir).values())
 
     # Reading the comment section and labeling substitution atom and numbering the ligands
     label_lig(input_ligands)
 
-    # Reading the comment section and labeling substitution atoms 
+    # Reading the comment section and labeling substitution atoms
     for core in input_cores:
         core.guess_bonds()
         label_core(core)
 
-    # Criteria for the minimal interatomic distances
-    min_dist = 1.2
-
     # Generate structures by combining ligands and cores
-    # If substitution is symmetric, produces equivalent structures, those should be deleted
     mono = substitution(input_ligands, input_cores, min_dist)
+    new_mols = []
+    new_mols.append(mono)
+    if n > 1:
+        for i in range(1,n):
+            poli = substitution(input_ligands, mono, min_dist)
+            new_mols.append(poli)
+            mono = poli
 
-    di = substitution(input_ligands, mono, min_dist)
-    di_unique = del_equiv_structures(di, 'linear')
-
-    tri = substitution(input_ligands, di, min_dist)
-
-    tetra = substitution(input_ligands, tri, min_dist)
-
-    tetra_unique = del_equiv_structures(tetra, 'D2h')
+    if symmetry == []:
+        pass
+    elif 'linear' in symmetry:
+        new_mols[1] = del_equiv_structures(new_mols[1], 'linear')
+    elif 'triangle' in symmetry:
+        new_mols[2] = del_equiv_structures(new_mols[2], 'triangle')
+    elif 'D2h' in symmetry:
+        new_mols[3] = del_equiv_structures(new_mols[2], 'D2h')
 
     # Combine and flatten all new molecules into a generator
-    ret = chain.from_iterable([mono, di_unique, tri, tetra_unique])
+    ret = chain.from_iterable(new_mols)
 
-    end = time.time()
-    print("Elapsed wall-clock time:  ", end - start)
     return ret
 
 
@@ -87,12 +94,27 @@ def export_dyes(mol_list: Iterable[Molecule],
                 new_dir: str = 'new_molecules',
                 err_dir: str = 'err_molecules',
                 min_dist: float = 1.2) -> None:
+    """Exports molecular coordinates to .xyz files
+
+    Parameters
+    ----------
+    mol_list: Iterable[Molecule]
+        List of molecules
+    new_dir: str
+        Name of the durectory to place new structures
+    err_dir: str
+        Name of the directory for molecules that do not fullfill min_dist criterion
+    min_dist: float
+        Criterion for the minimal interatomic distances
+
+    """
     if not exists(new_dir):
         os.makedirs(new_dir)
     if not exists(err_dir):
         os.makedirs(err_dir)
 
     # Export molecules to folders depending on the minimum core/ligand distance
+    print(mol_list)
     for mol in mol_list:
         name = mol.properties.name
         mol_distance = mol.properties.min_distance
@@ -100,7 +122,5 @@ def export_dyes(mol_list: Iterable[Molecule],
         if mol_distance > min_dist:
             mol.write(join(new_dir, f'{name}.xyz'))
         else:
-            # if minimal distance is smaler than given criteria,
-            # place structures in different folder
             mol.write(join(err_dir, f'err_{name}.xyz'))
             print(f"{name}: \n Minimal distance {mol_distance} A is smaller than {min_dist} A")
