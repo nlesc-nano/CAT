911ad2a30a8209a5f9d67aa27ece63246126af00
Bas van Beek <b.f.van.beek@vu.nl>
2021-11-18 15:51:33 +0100

    REL: CAT version bump: 0.10.0 -> 0.10.1

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ copyright = f'{_year}, {author}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the built documents.
-release = '0.10.0'  # The full version, including alpha/beta/rc tags.
+release = '0.10.1'  # The full version, including alpha/beta/rc tags.
 version = release.rsplit('.', maxsplit=1)[0]
 
 
